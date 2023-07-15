3419e6b845dbb531d4e1a765f778891c2f4a7d39
questaoresponse <contaprogramarv@example.com>
2023-07-15 19:45:57 -0300

    adicionar pasta

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from flask import Flask,render_template
+from flask_socketio import SocketIO,emit
 app=Flask('app')
+socketio=SocketIO(app)
 @app.route('/')
 def inicio():
     return render_template('index.html')
+@socketio.on('teste')
+def teste(data):
+    emit('enviado','mensagem')
 if __name__=='__main__':
-    app.run()
+    socketio.run(app)
