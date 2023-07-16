51d473d9c0c0aeb71eb0bd349e3c2582ac2ca95b
questaoresponse <contaprogramarv@example.com>
2023-07-16 13:37:53 -0300

    adicionar pasta

--- app.py
+++ app.py
@@ -8,5 +8,8 @@ def inicio():
 @socketio.on('teste')
 def teste(data):
     emit('enviado','mensagem')
+@socketio.on('ping')
+def socket(data):
+  emit('ping',data)
 if __name__=='__main__':
     socketio.run(app)
