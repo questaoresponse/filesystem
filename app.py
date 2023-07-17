5eded605f0b46bdfd0c4f6717b3f1b2811968c5e
questaoresponse <contaprogramarv@example.com>
2023-07-17 12:05:05 -0300

    adicionar pasta

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask,render_template,redirect,request,session
 from flask_session import Session
 from flask_socketio import SocketIO,emit
 import sqlite3
+import subprocess
+import os
 app=Flask('app')
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
@@ -58,6 +60,19 @@ def cadastro():
                 return "true"
             else:
                 return "false"
+@app.route('/teste_server')
+def teste_server():
+
+  def clone_repository_and_replace_folder(repository_url, folder_path):
+    # Clonar o repositório
+    subprocess.run(["git", "clone", repository_url])
+    repository_name = repository_url.split("/")[-1].split(".git")[0]
+    os.replace(repository_name, folder_path)
+    subprocess.run(["python","build_app.py"])
+    print("Repositório clonado e pasta substituída com sucesso!")
+  repository_url = "https://github.com/questaoresponse/pwa.git"  # URL do repositório a ser clonado
+  folder_path = os.getcwd()  # Caminho da pasta atual onde o arquivo Python está sendo executado
+  clone_repository_and_replace_folder(repository_url, folder_path)
 @socketio.on('teste')
 def teste(data):
     emit('enviado','mensagem')
