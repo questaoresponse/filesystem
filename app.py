3a8f2a9b66fd84e51e9a2cc0bc7d5d851c4a0a37
questaoresponse <contaprogramarv@example.com>
2023-07-17 11:28:20 -0300

    adicionar pasta

--- app.py
+++ app.py
@@ -1,18 +1,70 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,request,session
+from flask_session import Session
 from flask_socketio import SocketIO,emit
+import sqlite3
 app=Flask('app')
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 socketio=SocketIO(app)
+@app.before_request
+def before_request():
+    if not session.get('usuario'):
+        if request.path!='/cadastro' :
+            return redirect('/cadastro')
+    else:
+        if request.path=='/cadastro':
+            return redirect('/')
 @app.route('/')
 def inicio():
     return render_template('bem_vindo.html')
-@app.route('/sitepwa')
-def sitepwa():
-    return render_template('index.html')
+@app.route('/cadastro',methods=["GET","POST"])
+def cadastro():
+  if request.method=="GET":
+    return render_template("cadastro_login.html")
+  if request.method=="POST":
+    banco=sqlite3.connect("banco_usuarios.db")
+    cursor=banco.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS tabela_usuarios(usuario,email,senha)')
+    email=request.form.get('email')
+    tipo=request.form.get("tipo")
+    if tipo=="google":
+        usuario=email.split("@")[0]
+        senha="nenhum"
+        session['usuario']=usuario
+        cursor.execute('SELECT * FROM tabela_usuarios WHERE usuario=?',(usuario,))
+        usuarios=cursor.fetchall()
+        if len(usuarios)==0:
+           cursor.execute('INSERT INTO tabela_usuarios(usuario,email,senha) VALUES(?,?,?)',(usuario,email,senha))
+           banco.commit()
+        return 'true'
+    if tipo=="account":
+        usuario=request.form.get("usuario")
+        senha=request.form.get("senha")
+        tipo2=request.form.get("tipo2")
+        cursor.execute('SELECT * FROM tabela_usuarios WHERE usuario=?',(usuario,))
+        usuarios=cursor.fetchall()
+        if tipo2=="cadastro":
+            if len(usuarios)>0:
+                return "false"
+            else:
+                cursor.execute('INSERT INTO tabela_usuarios(usuario,email,senha) VALUES(?,?,?)',(usuario,email,senha))
+                banco.commit()
+                session["usuario"]=usuario
+                return "true"
+        if tipo2=="login":
+            if len(usuarios)>0:
+                session["usuario"]=usuario
+                return "true"
+            else:
+                return "false"
 @socketio.on('teste')
 def teste(data):
     emit('enviado','mensagem')
 @socketio.on('ping')
 def socket(data):
   emit('ping',data)
+from projetos.sitepwa.sitepwa import bp
+app.register_blueprint(bp)
 if __name__=='__main__':
     socketio.run(app)
--- projetos/sitepwa/sitepwa.py
+++ projetos/sitepwa/sitepwa.py
@@ -1,7 +1,12 @@
-from flask import render_template
-from app import app
-from app import socketio
+from flask import render_template,Blueprint
 namespace='/sitepwa'
-app.route(namespace)
+bp=Blueprint('sitepwa','app')
+@bp.route(namespace)
 def inicio():
+    socketio = bp.config['socketio']
+    receber_socketio(socketio)
     return render_template('index.html')
+def receber_socketio(socketio):
+    @socketio.on('ping')
+    def teste_ping(ping):
+        print(ping)
