18386e1d427e7d434048788ccbc1a1a5bb82579a
questaoresponse <contaprogramarv@example.com>
2023-07-17 13:04:11 -0300

    adicionar pasta

--- substituir.py
+++ substituir.py
@@ -1,6 +1,6 @@
 import subprocess
 def clonar_repositorio(url_repositorio, pasta_destino):
-    subprocess.run(["git", "clone", "--force", url_repositorio, pasta_destino])
+    subprocess.run(["git", "clone", "-f", url_repositorio, pasta_destino])
     subprocess.run(["python","build_app.py"])
     print("Repositório clonado e pasta raiz substituída com sucesso!")
 
