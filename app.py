7b2a13464f542b00f54b88cbaa2d81d4898ab7a3
questaoresponse <contaprogramarv@example.com>
2023-07-18 13:56:15 -0300

    adicionar pasta

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_socketio import SocketIO,emit
 import subprocess
 import sqlite3
 import os
+from admin.admin import before_request
 # Estabeleça a conexão
 # banco = mysql.connector.connect(
 #     user='seu_usuario',
@@ -14,10 +15,10 @@ import os
 # )
 # Crie o cursor
 # cursor = banco.cursor()
-app=Flask('app')
-# app.config["SESSION_PERMANENT"] = False
-app.secret_key='chave_para_teste'
 diretorio_atual = os.path.dirname(os.path.abspath(__file__))
+app=Flask('app',static_folder=diretorio_atual+'/static/')
+app.config["SESSION_PERMANENT"] = True
+app.secret_key='chave_para_teste'
 print(diretorio_atual)
 app.config['SESSION_FILE_DIR'] = f'{diretorio_atual}/flask_session'
 app.config['SESSION_COOKIE_PATH'] =f"{diretorio_atual}/flask_session"
@@ -27,14 +28,26 @@ socketio=SocketIO(app)
 def adicionar():
     os.getcwd()
     subprocess.run(["python",f"{diretorio_atual}/adc_gt.py"])
+    # pass
+ips_bloqueados = [ '10.0.0.1']
 @app.before_request
-def before_request():
-    if not session.get('usuario'):
-        if request.path!='/cadastro':
-            return redirect('/cadastro')
-    else:
-        if request.path=='/cadastro':
-            return redirect('/')
+def before_request_app():
+# Função para verificar se um IP está bloqueado
+ allowed_methods = ['GET', 'POST']  # Métodos permitidos
+ if request.method in allowed_methods and not request.path.startswith('/templates') and not request.path.startswith('/static') and not request.path.startswith('/socket'):
+  def ip_esta_bloqueado(ip):
+    return ip in ips_bloqueados
+  ip_cliente = request.remote_addr
+  print(ip_cliente)
+  if ip_esta_bloqueado(ip_cliente):
+        return render_template('ip_bloqueado.html')
+  if not session.get('usuario'):
+    if request.path!='/cadastro':
+        return redirect('/cadastro')
+  else:
+    if request.path=='/cadastro':
+        return redirect('/')
+    before_request()
 @app.route('/')
 def inicio():
     return render_template('bem_vindo.html')
@@ -43,33 +56,36 @@ def cadastro():
   if request.method=="GET":
     return render_template("cadastro_login.html")
   if request.method=="POST":
+    status=""
     banco=sqlite3.connect(diretorio_atual+'/usuarios.db')
     cursor=banco.cursor()
-    cursor.execute('CREATE TABLE IF NOT EXISTS tabela_usuarios(usuario,email,senha)')
+    cursor.execute('CREATE TABLE IF NOT EXISTS tabela_usuarios(usuario,email,senha,status,tipo)')
     email=request.form.get('email')
     tipo=request.form.get("tipo")
     if tipo=="google":
         usuario=email.split("@")[0]
         senha="nenhum"
         session['usuario']=usuario
-        cursor.execute('SELECT * FROM tabela_usuarios WHERE usuario=?',(usuario,))
+        cursor.execute('SELECT * FROM tabela_usuarios WHERE usuario=? and tipo="google"',(usuario,))
         usuarios=cursor.fetchall()
         if len(usuarios)==0:
-           cursor.execute('INSERT INTO tabela_usuarios(usuario,email,senha) VALUES(?,?,?)',(usuario,email,senha))
+           cursor.execute('INSERT INTO tabela_usuarios(usuario,email,senha,status,tipo) VALUES(?,?,?,?,?)',(usuario,email,senha,status,"google"))
            banco.commit()
            adicionar()
         return 'true'
     if tipo=="account":
         usuario=request.form.get("usuario")
         senha=request.form.get("senha")
+        if email=="contaprogramarv@gmail.com" and senha=="6626070":
+            status="creator"
         tipo2=request.form.get("tipo2")
-        cursor.execute('SELECT * FROM tabela_usuarios WHERE usuario=?',(usuario,))
+        cursor.execute('SELECT * FROM tabela_usuarios WHERE usuario=? and tipo="account"',(usuario,))
         usuarios=cursor.fetchall()
         if tipo2=="cadastro":
             if len(usuarios)>0:
                 return "false"
             else:
-                cursor.execute('INSERT INTO tabela_usuarios(usuario,email,senha) VALUES(?,?,?)',(usuario,email,senha))
+                cursor.execute('INSERT INTO tabela_usuarios(usuario,email,senha,status,tipo) VALUES(?,?,?,?,?)',(usuario,email,senha,status,tipo))
                 banco.commit()
                 session["usuario"]=usuario
                 adicionar()
@@ -89,5 +105,8 @@ def socket(data):
   emit('ping',data)
 from projetos.sitepwa.sitepwa import bp
 app.register_blueprint(bp)
+from admin.admin import bp
+app.register_blueprint(bp)
+
 if __name__=='__main__':
     socketio.run(app)
