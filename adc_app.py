134318ccad8412e5ed586b1020418cecda50d943
questaoresponse <contaprogramarv@example.com>
2023-07-18 22:54:15 -0300

    adicionar pasta

--- adc_app.py
+++ adc_app.py
@@ -1,8 +1,8 @@
 import subprocess
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-subprocess.run(["git","--config","user.email","contaprogramarv@gmail.com"])
-subprocess.run(["git","--config","user.name","questaoresponse"])
+subprocess.run(["git","config","--global","user.email","contaprogramarv@gmail.com"])
+subprocess.run(["git","config","--global","user.name","questaoresponse"])
 subprocess.run(["git","add","."])
 subprocess.run(["git","commit","-m","adicionar pasta"])
 subprocess.run(["git","push","-u","https://github.com/questaoresponse/filesystem.git","main"])
