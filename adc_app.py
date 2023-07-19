65ece3045125ea21b46b4ce22b2b7fcf0426670f
questaoresponse <contaprogramarv@gmail.com>
2023-07-19 10:46:42 -0300

    adicionar pasta

--- adc_app.py
+++ adc_app.py
@@ -1,8 +1,9 @@
 import subprocess
 import os
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+diretorio_atual=os.path.dirname(os.path.abspath(__file__))
+os.chdir(diretorio_atual)
 subprocess.run(["git","config","--global","user.email","contaprogramarv@gmail.com"])
 subprocess.run(["git","config","--global","user.name","questaoresponse"])
-subprocess.run(["git","add","."])
+subprocess.run(["git","add",diretorio_atual+"/flask_session",diretorio_atual+"/usuarios.db","adc_app.py"])
 subprocess.run(["git","commit","-m","adicionar pasta"])
 subprocess.run(["git","push","-u","https://github.com/questaoresponse/filesystem.git","main"])
